7ff239ccf3b3d27a5f16af8c2036e0e019755124
Bitajune <beheshtibita@gmail.com>
2020-07-13 20:10:10 -0700

    primative objects

--- script.js
+++ script.js
@@ -1,40 +1,37 @@
-// // // Function constructor 
+// Function constructor
+/*
 
-// // let john = {
-// //     name: 'John',
-// //     yearOfBirth: 1990,
-// //     job: 'teacer'
-// // };
+let Person = function (name, yearOfBirth, job) {
+    this.name = name;
+    this.yearOfBirth = yearOfBirth;
+    this.job = job;
+};
 
+Person.prototype.calculateAge = function () {
+    console.log(2020 - this.yearOfBirth);
+};
 
-// let Person = function (name, yearOfBirth, job) {
-//     this.name = name;
-//     this.yearOfBirth = yearOfBirth;
-//     this.job = job;
-// };
+Person.prototype.lastName = 'Smith';
 
-// Person.prototype.calculateAge = function () {
-//     console.log(2020 - this.yearOfBirth);
-// };
-
-// Person.prototype.lastName = 'Smith';
-
-// let john = new Person('John', 1990, 'teacher');
-// let jane = new Person('Jane', 1969, 'designer');
-// let mark = new Person('Mark', 1948, 'retired');
+let john = new Person('John', 1990, 'teacher');
+let jane = new Person('Jane', 1969, 'designer');
+let mark = new Person('Mark', 1948, 'retired');
 
 
-// john.calculateAge();
-// jane.calculateAge();
-// mark.calculateAge();
+john.calculateAge();
+jane.calculateAge();
+mark.calculateAge();
 
-// console.log(john.lastName);
-// console.log(jane.lastName);
-// console.log(mark.lastName);
+console.log(john.lastName);
+console.log(jane.lastName);
+console.log(mark.lastName);
+*/
+
 
 
 
 // Object.create method
+/*
 let personProto = {
     calculateAge: function () {
         console.log(2020 - this.yearOfBirth);
@@ -56,3 +53,54 @@ let jane = Object.create(personProto,
 
 console.log(john);
 console.log(jane);
+*/
+
+
+
+
+
+// Primatives vs objects
+
+
+// Primatives
+/*
+let a = 13;
+let b = a;
+a = 27;
+console.log(a, b);
+
+// Objects
+let objectOne = {
+    name: 'John',
+    age: 25
+};
+let objectTwo = objectOne;
+objectOne.age = 30;
+
+console.log(objectOne.age, objectTwo.age);
+
+
+// Functions
+let age = 28;
+let obj = {
+    name: 'Bita',
+    city: 'Tustin'
+};
+
+function change(a, b) {
+    a = 30;
+    b.city = 'Los Angeles'
+}
+change(age, obj)
+
+console.log(age, obj.city);
+*/
+
+let letters = ['a', 'b', 'c'];
+let lettersTwo = letters;
+letters = ['x', 'y', 'z'];
+
+console.log(letters, lettersTwo)
+
+
+
