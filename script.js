32a361c23a3fe0ff07f5a6c7cb34701282b9f64f
Bitajune <beheshtibita@gmail.com>
2020-07-13 19:40:16 -0700

    object create method

--- script.js
+++ script.js
@@ -1,33 +1,58 @@
-// // Function constructor 
+// // // Function constructor 
 
-// let john = {
-//     name: 'John',
-//     yearOfBirth: 1990,
-//     job: 'teacer'
+// // let john = {
+// //     name: 'John',
+// //     yearOfBirth: 1990,
+// //     job: 'teacer'
+// // };
+
+
+// let Person = function (name, yearOfBirth, job) {
+//     this.name = name;
+//     this.yearOfBirth = yearOfBirth;
+//     this.job = job;
 // };
 
+// Person.prototype.calculateAge = function () {
+//     console.log(2020 - this.yearOfBirth);
+// };
 
-let Person = function (name, yearOfBirth, job) {
-    this.name = name;
-    this.yearOfBirth = yearOfBirth;
-    this.job = job;
+// Person.prototype.lastName = 'Smith';
+
+// let john = new Person('John', 1990, 'teacher');
+// let jane = new Person('Jane', 1969, 'designer');
+// let mark = new Person('Mark', 1948, 'retired');
+
+
+// john.calculateAge();
+// jane.calculateAge();
+// mark.calculateAge();
+
+// console.log(john.lastName);
+// console.log(jane.lastName);
+// console.log(mark.lastName);
+
+
+
+// Object.create method
+let personProto = {
+    calculateAge: function () {
+        console.log(2020 - this.yearOfBirth);
+    }
 };
 
-Person.prototype.calculateAge = function () {
-    console.log(2020 - this.yearOfBirth);
-};
-
-Person.prototype.lastName = 'Smith';
-
-let john = new Person('John', 1990, 'teacher');
-let jane = new Person('Jane', 1969, 'designer');
-let mark = new Person('Mark', 1948, 'retired');
+let john = Object.create(personProto);
+john.name = 'John';
+john.yearOfBirth = 1990;
+john.job = 'teacher';
 
 
-john.calculateAge();
-jane.calculateAge();
-mark.calculateAge();
+let jane = Object.create(personProto,
+    {
+        name: { value: 'Jane' },
+        yearOfBirth: { value: 1969 },
+        job: { value: 'designer' }
+    });
 
-console.log(john.lastName);
-console.log(jane.lastName);
-console.log(mark.lastName);
+console.log(john);
+console.log(jane);
