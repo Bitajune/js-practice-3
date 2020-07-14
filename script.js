1f01742e66934a1cd3471357f2fc30f9cb0bb34f
Bitajune <beheshtibita@gmail.com>
2020-07-13 19:23:55 -0700

    inheritance

--- script.js
+++ script.js
@@ -11,10 +11,13 @@ let Person = function (name, yearOfBirth, job) {
     this.name = name;
     this.yearOfBirth = yearOfBirth;
     this.job = job;
-    this.calculateAge = function () {
-        console.log(2020 - this.yearOfBirth);
-    }
 };
+
+Person.prototype.calculateAge = function () {
+    console.log(2020 - this.yearOfBirth);
+};
+
+Person.prototype.lastName = 'Smith';
 
 let john = new Person('John', 1990, 'teacher');
 let jane = new Person('Jane', 1969, 'designer');
@@ -25,9 +28,6 @@ john.calculateAge();
 jane.calculateAge();
 mark.calculateAge();
 
-
-
-
-
-
-
+console.log(john.lastName);
+console.log(jane.lastName);
+console.log(mark.lastName);
